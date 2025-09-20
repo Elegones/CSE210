ff9a411c9f0f53c13979eae35470804c4fc4cc89
Elegones <ethan.legones@gmail.com>
2025-09-20 17:51:21 -0600

    Grade code

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -8,6 +8,6 @@ class Program
         string name = Console.ReadLine();
         Console.WriteLine("Hello What is your last name?");
         string lname = Console.ReadLine();
-        Console.WriteLine($"your name is {lname}, {name}{lname}.");
+        Console.WriteLine($"your name is {lname}, {name} {lname}.");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Hi, what is you grade");
+        string lGrade = Console.ReadLine();
+
+        int nGrade = int.Parse(lGrade);
+
+        string let = "N";
+
+        if (nGrade > 90)
+        {
+            
+            let = "A";
+        }
+
+        else if (nGrade > 80)
+        {
+            
+            let = "B";
+        }
+
+        else if (nGrade > 70)
+        {
+         
+            let = "C";
+        }
+           else if (nGrade > 60)
+        {
+          
+            let = "D";
+        }
+        else
+        {
+          
+            let = "F";
+        }
+
+
+        if (let == "A" || let == "B" || let == "C")
+        {
+            Console.WriteLine($"Good job you pass the class, your grade is a {let}");
+        }
+        else 
+        {
+        Console.WriteLine($"Better luck next time, you did not pass, your grade is a {let}");
+        }
+
     }
 }
