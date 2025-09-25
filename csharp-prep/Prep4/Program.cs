3533030ffd07c2fad06135f30c7cf692cdbf2f96
Elegones <ethan.legones@gmail.com>
2025-09-25 13:41:34 -0600

    Finished

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,27 +1,34 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-         List<int> numberList = new List<int>();
+        List<int> numberList = new List<int>();
         Console.WriteLine("Enter any numbers you wish to add to the list, enter 0 when your finished");
         int numList = 1;
         do
         {
             string newNum = Console.ReadLine();
-             numList = int.Parse(newNum);
+            numList = int.Parse(newNum);
 
-            
+
             numberList.Add(numList);
 
 
-        } while (numList!=0);
+        } while (numList != 0);
 
-        for (int i = 0; i < numberList.Count; i++) 
-        {
-            Console.WriteLine(numberList[i]);
-        }
+       
+        int total = numberList.Sum();
+        Console.WriteLine($"The sum is {total}");
+
+        double ave = numberList.Average();
+        Console.WriteLine($"the average is {ave}");
+
+        int big = numberList.Max();
+         Console.WriteLine($"the largest numebr entered is {big}");
+       
 
     }
 }
