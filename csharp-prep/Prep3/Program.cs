fae030d6bc7b8f7943a3f29becc250efda20c85c
Elegones <ethan.legones@gmail.com>
2025-09-24 23:29:47 -0600

    Magic number game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,54 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+
+
+        int num = 0;
+
+        int answer = 0;
+
+        
+        
+
+        Random randomGenerator = new Random();
+        num = randomGenerator.Next(1, 100);
+
+        do
+        {
+
+            Console.WriteLine("what is the magic number");
+
+            string Anum = Console.ReadLine();
+
+            answer = int.Parse(Anum);
+
+            if (answer > num)
+            {
+                Console.WriteLine("Answer is to High");
+
+            }
+
+            else if (answer < num)
+            {
+                Console.WriteLine("Answer is to Low");
+
+            }
+
+            else
+            {
+
+                Console.WriteLine($"Thats correct");
+            }
+
+
+
+
+        } while (answer != num);
+
+        }
+    
 }
