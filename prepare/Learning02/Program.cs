8a05343b3449136143f37bd0bffe77139b31124a
Elegones <ethan.legones@gmail.com>
2025-09-30 13:35:13 -0600

    test

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning02 World!");
+        School school1= new School();
+        school.name="BYU-I";
+        school.numStudents= 32000;
+         school.numTeachers= 500;
+        
+        school1.ShowDetails();
     }
 }
---
+++ prepare/Learning02/School.cs
@@ -0,0 +1,26 @@
+using System;
+
+public class School
+{
+    public int numStudents;
+
+    public int numTeachers;
+
+    public string name;
+
+
+    public void Register()
+    {
+
+
+    }
+
+
+    public void ShowDeteails()
+    {
+        Console.WriteLine($"School:{name} has {numStudents} students and {numTeachers} Teachers");
+
+    }
+
+
+}
