d11e65b96fdf6c7095660d23b6a23a756cd389ac
Elegones <ethan.legones@gmail.com>
2025-10-02 22:04:48 -0600

    L2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,12 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
-        School school1= new School();
-        school1.name="BYU-I";
-        school1.numStudents= 32000;
-         school1.numTeachers= 500;
-        
-        school1.ShowDetails();
+      
     }
 }
